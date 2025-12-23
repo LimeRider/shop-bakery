85a2f01303bf06f5f8249a160915d5801a0dbd2a
LimeRider <cgakaviy@gmail.com>
2025-12-23 23:46:20 +0200

    do a lot

--- modal.js
+++
@@ -1,8 +0,0 @@
-const modal = document.querySelector(".backdrop");
-const openModalButton = document.querySelector(".modal-button-open");
-const closeModalButton = document.querySelector(".modal-button-close");
-
-const toggleModal = () => modal.classList.toggle("is-hidden");
-
-openModalButton.addEventListener("click", toggleModal);
-closeModalButton.addEventListener("click", toggleModal);
